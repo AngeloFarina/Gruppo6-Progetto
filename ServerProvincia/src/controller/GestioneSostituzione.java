fd0b05084b240bc24e6b275a934831d855757a8d
Angelo Farina <angi.farina@gmail.com>
2019-05-30 15:45:57 +0200

    aggiornamento modifica mezzi, elimina mezzo + controller collegati

--- ServerProvincia/src/controller/GestioneSostituzione.java
+++ ServerProvincia/src/controller/GestioneSostituzione.java
@@ -1,11 +1,19 @@
 package controller;
 
+import java.sql.Connection;
+
 import interfacce.IGestioneSostituzione;
 import model.Mezzo;
 import model.RichiestaSostituzione;
 
 public class GestioneSostituzione implements IGestioneSostituzione {
 
+	private Connection db;
+	
+	public GestioneSostituzione(Connection db) {
+		this.db = db;
+	}
+	
 	@Override
 	public void effettuaSostituzione(Mezzo m, RichiestaSostituzione s) {
 		
