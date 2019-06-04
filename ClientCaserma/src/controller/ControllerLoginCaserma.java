c33c2b599839cd8669911bf77946ab0d495f2e56
Paolo Verdini <paolopiano1997@gmail.com>
2019-06-04 22:37:43 +0200

    Controller login

---
+++ ClientCaserma/src/controller/ControllerLoginCaserma.java
@@ -0,0 +1,92 @@
+package controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import interfacceGrafiche.InterfacciaCapoSquadra;
+import interfacceGrafiche.InterfacciaVigile;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class ControllerLoginCaserma {
+	private static final int BROKERPORT = 1051;
+
+	public ControllerLoginCaserma() {
+		
+	}
+	
+	public void richiestaLogin(String username,String password, Window window) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException {
+		Socket clientSocket = new Socket("localhost",BROKERPORT);
+		System.out.println("ClientLogin: creata Socket: " +clientSocket.getLocalSocketAddress());
+		DataOutputStream outSock = new DataOutputStream(clientSocket.getOutputStream());
+		ObjectOutputStream outObj = new ObjectOutputStream(clientSocket.getOutputStream());
+		ObjectInputStream inObj = new ObjectInputStream(clientSocket.getInputStream());
+		outSock.writeUTF("localhost");
+		outSock.writeUTF("localhost");
+		outSock.writeUTF("login");
+		List<String> param = new ArrayList<String>();
+		param.add(username);
+		param.add(password);
+		outObj.writeObject(param);
+		String ruolo,nome,id;
+		System.out.println("Sto per ricevere i parametri");
+		param = new ArrayList<String>((List<String>)inObj.readObject());
+		ruolo = param.get(2);
+		nome = param.get(3);
+		id = param.get(4);
+		System.out.println("Parametri ricevuti " + ruolo + " " + nome + " " + id);
+		if(ruolo.equals("caposquadra"))
+			setNewStageCapoSquadra(window,nome,id);
+		else if(ruolo.equals("vigile"))
+			setNewStageVigile(window,nome,id);
+	}
+
+	private void setNewStageVigile(Window window,String nome, String id) {
+		Stage s = (Stage) window;
+		s.close();
+		s.setTitle("Gestione Mezzi Vigili del Fuoco");
+		InterfacciaVigile root = null;
+		try {
+			root = new InterfacciaVigile(new ControllerClientCaserma(id,nome));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		setScene(s,root);
+	}
+
+	private void setNewStageCapoSquadra(Window window, String nome,String id) {
+		Stage s = (Stage) window;
+		s.close();
+		s.setTitle("Gestione Mezzi Vigili del Fuoco");
+		InterfacciaCapoSquadra root = null;
+		try {
+			root = new InterfacciaCapoSquadra(new ControllerClientCaserma(id,nome));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		setScene(s, root);
+		
+	}
+
+	private void setScene(Stage s, Pane root) {
+		root.setPrefSize(1280, 720);
+		Scene scene = new Scene(root, 1280,720,Color.WHITE);
+		s.setScene(scene);
+		s.setResizable(false);
+		s.show();
+	}
+	
+}
